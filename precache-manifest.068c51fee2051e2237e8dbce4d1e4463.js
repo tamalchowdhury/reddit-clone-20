944e9dc4f89be7cbd0cbe4730bac0f623ccc7723
Tamal Anwar <tamalanwaronline@gmail.com>
2019-02-11 22:50:54 +0600

    Updates

---
+++ precache-manifest.068c51fee2051e2237e8dbce4d1e4463.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "13a8bbcc57005238b90b7d944bb88b04",
+    "url": "/reddit-clone-20/static/media/sprite.13a8bbcc.png"
+  },
+  {
+    "revision": "db14b8ef4ac4d70520a7",
+    "url": "/reddit-clone-20/static/js/runtime~main.db14b8ef.js"
+  },
+  {
+    "revision": "202a2631ed62e6cdb912",
+    "url": "/reddit-clone-20/static/js/main.202a2631.chunk.js"
+  },
+  {
+    "revision": "563e21863cf2e4f558d6",
+    "url": "/reddit-clone-20/static/js/1.563e2186.chunk.js"
+  },
+  {
+    "revision": "202a2631ed62e6cdb912",
+    "url": "/reddit-clone-20/static/css/main.c47f1046.chunk.css"
+  },
+  {
+    "revision": "3e21172586c8c299ae392868fbdeb7a6",
+    "url": "/reddit-clone-20/index.html"
+  }
+];
